ca693e2758828a9165a93ad8c432cdf7ba60ae9a
Daniel <daniel.kulesza@mail.utoronto.ca>
2016-10-18 17:06:48 -0400

    Finished resources assignment. Support for different main layouts for portrait and landscape tablets and phones.

---
+++ app/src/main/java/scs2682/androidsession5hw/AppActivity.java
@@ -0,0 +1,13 @@
+package scs2682.androidsession5hw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.appactivity);
+    }
+}
